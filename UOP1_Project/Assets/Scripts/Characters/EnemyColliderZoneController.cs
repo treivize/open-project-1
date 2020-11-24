2c28454eb4dcd23d4805e8e51e09334f36d85dbb
treivize <ypuyhaubert@gmail.com>
2020-11-24 23:58:53 +0100

    Added Plant Critter initial states conditions and transitions

---
+++ UOP1_Project/Assets/Scripts/Characters/EnemyColliderZoneController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyColliderZoneController : MonoBehaviour
+{
+	[SerializeField] private VoidEventChannelSO _enterZone = default;
+
+	[SerializeField] private VoidEventChannelSO _exitZone = default;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.GetComponent<Protagonist>() != null)
+		{
+			_enterZone.RaiseEvent();
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.GetComponent<Protagonist>() != null)
+		{
+			_exitZone.RaiseEvent();
+		}
+	}
+}
---
+++ UOP1_Project/Assets/Scripts/Characters/StateMachine/Conditions/HasReceivedEventSO.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UOP1.StateMachine;
+using UOP1.StateMachine.ScriptableObjects;
+
+[CreateAssetMenu(fileName = "HasReceivedEvent", menuName = "State Machines/Conditions/Has Received Event")]
+public class HasReceivedEvent : StateConditionSO<HasHitHeadCondition> {
+
+	[SerializeField] VoidEventChannelSO _voidEvent;
+	protected override Condition CreateCondition() => new HasReceivedEventCondition(_voidEvent);
+}
+
+public class HasReceivedEventCondition : Condition
+{
+	private VoidEventChannelSO _voidEvent;
+	private bool _eventTriggered;
+
+	public override void Awake(StateMachine stateMachine)
+	{
+		_eventTriggered = false;
+		_voidEvent.OnEventRaised += EventReceived;
+	}
+
+	public HasReceivedEventCondition(VoidEventChannelSO voidEvent)
+	{
+		_voidEvent = voidEvent;
+	}
+
+	protected override bool Statement()
+	{
+		return _eventTriggered;
+	}
+
+	private void EventReceived()
+	{
+		_eventTriggered = true;
+	}
+}
